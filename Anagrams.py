1e05717a281074f89692a0c6dda471e814c12f87
Saarthak Sangamnerkar <31529049+saarthaksangam@users.noreply.github.com>
2020-06-01 19:49:39 +0530

    Update Anagrams.py

--- Anagrams.py
+++ Anagrams.py
@@ -1,37 +1,9 @@
+from collections import Counter
 def number_needed(a, b):
-    s = 0
-    
-    maxm=""
-    minn=""
-    
-    if max(len(a),len(b)) == len(b):
-        
-        for k in b:
-            maxm = maxm + k
-        for n in a:
-            minn = minn + n
-        
-    else:
-        
-        for k in a:
-            maxm = maxm + k
-        for n in b:
-            minn = minn + n
-    
-    
-    
-    for i in range(len(maxm)):
-        for j in range(len(minn)):
-            
-            if maxm[i] == minn[j] and minn[j] != "X":
-                s += 1
-                c = list(minn)
-                c[j] = "X"
-                minn="".join(c)
-                break
-    print maxm
-    print minn
-    return len(a)+len(b)-(s*2)
+    ct_a = Counter(a)
+    ct_b = Counter(b)
+    ct_a.subtract(ct_b)
+    return sum(abs(i) for i in ct_a.values())
     
     
     
